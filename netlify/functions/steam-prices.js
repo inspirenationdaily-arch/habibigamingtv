f01722200dd44fc452cca095639b8ed21d5d820d
inspirenationdaily-arch <inspirenationdaily@gmail.com>
2026-03-31 21:22:42 -0500

    Update steam-prices.js

--- netlify/functions/steam-prices.js
+++ netlify/functions/steam-prices.js
@@ -1,41 +1,21 @@
 exports.handler = async function () {
   const apiKey = process.env.STEAMWEBAPI_KEY;
 
-  const skins = [
-    "AWP | Dragon Lore (Field-Tested)",
-    "AK-47 | Fire Serpent (Field-Tested)",
-    "M4A4 | Howl (Field-Tested)",
-    "AK-47 | Wild Lotus (Factory New)",
-    "AWP | Gungnir (Factory New)",
-    "AK-47 | Case Hardened (Factory New)",
-    "Glock-18 | Fade (Factory New)",
-    "M4A1-S | Knight (Factory New)",
-    "AWP | Medusa (Factory New)",
-    "AK-47 | Bloodsport (Factory New)"
-  ];
+  const url =
+    "https://www.steamwebapi.com/steam/api/items" +
+    "?key=" + encodeURIComponent(apiKey) +
+    "&game=cs2";
 
   try {
-    const results = [];
-
-    for (const name of skins) {
-      const url =
-        "https://www.steamwebapi.com/steam/api/item" +
-        "?key=" + encodeURIComponent(apiKey) +
-        "&game=cs2" +
-       "&market_hash_name=" + encodeURIComponent(name);
-
-      const response = await fetch(url);
-      const data = await response.json();
-
-      results.push(data);
-    }
+    const response = await fetch(url);
+    const data = await response.json();
 
     return {
       statusCode: 200,
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify(results)
+      body: JSON.stringify(data.slice(0, 20)) // limit returned data
     };
   } catch (error) {
     return {
